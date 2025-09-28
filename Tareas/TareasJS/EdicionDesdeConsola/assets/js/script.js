c39cf6d7e07026c364a7a779d24ddc14a128b41f
NicoBadenes <nicolasbadenes@gmail.com>
2025-09-27 22:40:20 -0300

    Update script.js

--- Tareas/TareasJS/EdicionDesdeConsola/assets/js/script.js
+++ Tareas/TareasJS/EdicionDesdeConsola/assets/js/script.js
@@ -1,37 +1,37 @@
-// 1. Aplicar clase de color a h1
-// document.getElementById('titulo').className = 'rojo';
+//  1. Aplicar clase de color a h1
+// document.querySelector('#titulo').className = 'rojo';
 
-// 2. Aplicar tamaño de letra a párrafo
-// document.getElementById('parrafo').className = 'grande';
+//  2. Aplicar tamaño de letra a párrafo
+// document.querySelector('#parrafo').className = 'grande';
 
-// 3. Aplicar fondo a caja
-// document.getElementById('mi-caja').className += ' fondo-amarillo';
+//  3. Aplicar fondo a caja
+// document.querySelector('.caja').classList.add('fondo-amarillo'); 
 
-// 4. Resaltar h2
-// document.getElementById('subtitulo').classList.add('resaltar');
-// document.getElementById('subtitulo').classList.remove('resaltar');
+//  4. Resaltar h2
+// document.querySelector('#subtitulo').classList.add('resaltar');
+// document.querySelector('#subtitulo').classList.remove('resaltar');
 
-// 5. Verificar clase activo
-// if(document.getElementById('mi-caja').classList.contains('activo')) {
+//  5. Verificar clase activo
+// if(document.querySelector('.caja').classList.contains('activo')) {
 //   console.log("El div ya tiene la clase activo");
 // } else {
 //   console.log("El div no tiene la clase activo");
 // }
 
-// 6. Colores para mensaje
-// document.getElementById('mensaje').className = 'violeta';
+//  6. Colores para mensaje
+// document.querySelector('#mensaje').className = 'violeta';
 
-// 7. Tamaño de h3
-// document.getElementById('titulo3').className = 'tamaño-grande';
+//  7. Tamaño de h3
+// document.querySelector('#titulo3').className = 'tamaño-grande';
 
-// 8. Estilo directo a cuadro
-// let cuadro = document.getElementById('cuadro');
+//  8. Estilo directo a cuadro
+// let cuadro = document.querySelector('.cuadro');
 // cuadro.style.backgroundColor = 'black';
 // cuadro.style.color = 'white';
 // cuadro.style.padding = '20px';
 
-// 9. Mover div movible
-// let movible = document.getElementById('movible');
+//  9. Mover div movible
+// let movible = document.querySelector('.movible');
 // movible.style.marginLeft = '50px';
 // movible.style.marginLeft = '100px';
 // movible.style.marginLeft = '150px';
